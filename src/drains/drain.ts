713d3ddc9c979d0540cbed7f101fb2ddbc759baf
Sebastian Vollbrecht <sebivo@freenet.de>
2025-05-05 14:07:27 +0200

    Refactor drain serializazion and deserialization

--- src/drains/drain.ts
+++ src/drains/drain.ts
@@ -4,25 +4,39 @@ import type { TestResults } from "../models/test-results-model.js";
  * A drain represents a service or tool to which test information can be pushed, such as Slack,
  * Microsoft Teams or simply the standard output.
  */
-export abstract class Drain<ConfigurationType, DrainOutput> {
+export abstract class Drain<Configuration, DrainDetails, DrainResult> {
   /**
    * The drain configuration. It is used during serialization and deserialization of the drain.
    */
-  protected readonly configuration: ConfigurationType;
+  protected readonly configuration: Configuration;
 
   /**
    * Constructs a new drain based on the configuration provided.
    *
    * @param configuration the drain configuration
    */
-  constructor(configuration: ConfigurationType) {
+  constructor(configuration: Configuration) {
     this.configuration = configuration;
   }
+
+  /**
+   * Returns the configuration of the drain instance.
+   *
+   * @returns the configuration
+   */
+  public getConfiguration(): Readonly<Configuration> {
+    return this.configuration;
+  }
+
   /**
    * Writes test results, containing one or more tests and their results.
    *
    * @param results the results to write
+   * @param details the drain details
    * @returns the drained result
    */
-  public abstract writeTestResults(results: TestResults): Promise<DrainOutput>;
+  public abstract writeTestResults(
+    results: TestResults,
+    details: DrainDetails
+  ): Promise<DrainResult>;
 }
