731bfee9201994f94d54538109a28739377e18b2
Sebastian Vollbrecht <sebivo@freenet.de>
2025-05-05 14:07:53 +0200

    Add Microsoft Teams drain handler

---
+++ src/drains/microsoft-teams/microsoft-teams-drain-handler.ts
@@ -0,0 +1,66 @@
+import { input } from "@inquirer/prompts";
+import { DrainHandler } from "../../cli/cli-drain-handler.js";
+import { getEnv } from "../../util/env.js";
+import type { MicrosoftTeamsDrainDetails } from "./microsoft-teams-drain.js";
+import { MicrosoftTeamsDrain } from "./microsoft-teams-drain.js";
+
+export class MicrosoftTeamsDrainHandler extends DrainHandler<
+  MicrosoftTeamsDrain,
+  SerializedConfiguration,
+  SerializedParameters
+> {
+  public buildDrain(): MicrosoftTeamsDrain {
+    return new MicrosoftTeamsDrain({});
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public serializeDrain(drain: MicrosoftTeamsDrain): SerializedConfiguration {
+    return {};
+  }
+
+  public deserializeDrain(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    serializedDrain: SerializedConfiguration
+  ): MicrosoftTeamsDrain {
+    return new MicrosoftTeamsDrain({});
+  }
+
+  public async buildDrainParameters(): Promise<MicrosoftTeamsDrainDetails> {
+    return {
+      incomingWebhookUrl: await this.getWebhookUrl(),
+    };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public serializeDrainParameters(parameters: MicrosoftTeamsDrainDetails): SerializedParameters {
+    return {};
+  }
+
+  public async deserializeDrainParameters(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    serializedParameters: SerializedParameters
+  ): Promise<MicrosoftTeamsDrainDetails> {
+    return {
+      incomingWebhookUrl: await this.getWebhookUrl(),
+    };
+  }
+
+  private async getWebhookUrl(): Promise<string> {
+    return (
+      getEnv("microsoft-teams-webhook-url", false) ??
+      (await input({
+        message: `What is the Webhook URL of your Microsoft Teams Channel?`,
+      }))
+    );
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface SerializedConfiguration {
+  // Nothing. Currently, nothing must be configured in a Microsoft Teams drain handler.
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface SerializedParameters {
+  // Nothing. Currently, only the web hook URL is required, which is private information.
+}
