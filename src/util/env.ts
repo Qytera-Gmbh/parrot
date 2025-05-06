8a9c30298370dd52ba3508db134f7b49f4fd6211
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2025-05-06 16:17:11 +0200

    Add better environment variable error handling

--- src/util/env.ts
+++ src/util/env.ts
@@ -10,11 +10,12 @@ type EnvironmentVariable =
   | "xray-url";
 
 /**
- * Returns an environment variable value for a specified environment variable. Throws an error if
- * the variable has not been set.
+ * Returns an environment variable value for a specified environment variable.
  *
  * @param kind the environment variable
  * @returns the value
+ *
+ * @throws if the variable is not defined
  */
 export function getEnv(kind: EnvironmentVariable): string;
 
@@ -22,42 +23,52 @@ export function getEnv(kind: EnvironmentVariable): string;
  * Returns an environment variable value for a specified environment variable.
  *
  * @param kind the environment variable
- * @returns the value or `undefined` if it has not been set
+ * @param throwIfUndefined whether to throw an error if the environment variable is undefined
+ * @returns the value
  */
-export function getEnv(kind: EnvironmentVariable, throwIfMissing: false): string | undefined;
-export function getEnv(kind: EnvironmentVariable, throwIfMissing?: false): string | undefined {
-  let value: string | undefined;
-  switch (kind) {
-    case "jira-email":
-      value = process.env.JIRA_EMAIL;
-      break;
-    case "jira-password":
-      value = process.env.JIRA_PASSWORD;
-      break;
-    case "jira-token":
-      value = process.env.JIRA_TOKEN;
-      break;
-    case "jira-url":
-      value = process.env.JIRA_URL;
-      break;
-    case "jira-username":
-      value = process.env.JIRA_USERNAME;
-      break;
-    case "microsoft-teams-webhook-url":
-      value = process.env.MICROSOFT_TEAMS_WEBHOOK_URL;
-      break;
-    case "xray-client-id":
-      value = process.env.XRAY_CLIENT_ID;
-      break;
-    case "xray-client-secret":
-      value = process.env.XRAY_CLIENT_SECRET;
-      break;
-    case "xray-url":
-      value = process.env.XRAY_URL;
-      break;
-  }
-  if (throwIfMissing !== false && !value) {
-    throw new Error(`Environment variable is undefined: ${kind}`);
+export function getEnv(kind: EnvironmentVariable, throwIfUndefined: boolean): string | undefined;
+export function getEnv(kind: EnvironmentVariable, throwIfUndefined?: boolean): string | undefined {
+  const name = getEnvName(kind);
+  const value = process.env[name];
+  if (!value && throwIfUndefined !== false) {
+    throw new Error(
+      [
+        `Environment variable is undefined: ${name}`,
+        "",
+        "Please perform one of the following steps to configure the testing project:",
+        `- add environment variable ${name} to your system's environemnt variables`,
+        `- create a .env file, append ${name}=<value> to it and pass it to Parrot using --env-file`,
+      ].join("\n")
+    );
   }
   return value;
+}
+
+/**
+ * Returns the name of the specified environment variable.
+ *
+ * @param kind the environment variable
+ * @returns its name
+ */
+function getEnvName(kind: EnvironmentVariable): string {
+  switch (kind) {
+    case "jira-email":
+      return "JIRA_EMAIL";
+    case "jira-password":
+      return "JIRA_PASSWORD";
+    case "jira-token":
+      return "JIRA_TOKEN";
+    case "jira-url":
+      return "JIRA_URL";
+    case "jira-username":
+      return "JIRA_USERNAME";
+    case "microsoft-teams-webhook-url":
+      return "MICROSOFT_TEAMS_WEBHOOK_URL";
+    case "xray-client-id":
+      return "XRAY_CLIENT_ID";
+    case "xray-client-secret":
+      return "XRAY_CLIENT_SECRET";
+    case "xray-url":
+      return "XRAY_URL";
+  }
 }
